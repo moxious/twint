a88e4de80d4c6268044633264b42bd672c850d1d
Moxious <moxious@oldhat.org>
2020-06-14 14:31:08 -0400

    improvements

--- import-tweets.py
+++ import-tweets.py
@@ -12,86 +12,89 @@ def load_tweets_in_batches(filename):
 
     fp = open(filename, "r")
 
-    line = 1
+    lineno = 1
     while True:
         try:
             line = fp.readline()
             if not line: break
             
             tweets.append(json.loads(line))
-            line = line + 1
+            lineno = lineno + 1
 
             if len(tweets) > MAX_BATCH_LENGTH:
                 batches.append(tweets)
                 tweets = []
         except Exception as e:
-            print("Error on line %d: %s" % (line, e))
+            print("Error on line %d of %s: %s" % (lineno, filename, e))
             break
 
     fp.close()
     batches.append(tweets)
     return batches
 
-
 LOAD_TWEETS_CYPHER = """
-UNWIND $events AS event
-MERGE (c:Conversation { id: event.conversation_id })
-MERGE (u:User { username: event.username })
-   ON CREATE SET u.id = toString(event.user_id), u.name = event.name
-   ON MATCH SET u.name = event.name
-MERGE (t:Tweet {
-    id: event.id
-})
-    ON CREATE SET
-        t.created = datetime(event.date + "T" + event.time + coalesce(event.timezone, "Z")),
-        t.tweet = event.tweet,
-        t.link = event.link,
-        t.retweet = event.retweet,
-        t.geo = event.geo,
-        t.source = event.source,
-        t.likes = event.likes_count,
-        t.retweets = event.retweets_count,
-        t.replies = event.replies_count
-MERGE (u)-[:TWEET]->(t)
-MERGE (t)-[:IN]->(c)
+    UNWIND $events AS event
+    MERGE (c:Conversation { id: event.conversation_id })
+    MERGE (u:User { username: event.username })
+    ON CREATE SET u.id = toString(event.user_id), u.name = event.name
+    ON MATCH SET u.name = event.name
+    MERGE (t:Tweet {
+        id: event.id
+    })
+        ON CREATE SET
+            t.created = datetime(event.date + "T" + event.time + coalesce(event.timezone, "Z")),
+            t.tweet = event.tweet,
+            t.link = event.link,
+            t.retweet = event.retweet,
+            t.geo = event.geo,
+            t.source = event.source,
+            t.likes = event.likes_count,
+            t.retweets = event.retweets_count,
+            t.replies = event.replies_count
+    MERGE (u)-[:TWEET]->(t)
+    MERGE (t)-[:IN]->(c)
 
-WITH t, event
-UNWIND event.hashtags as hashtag
-MERGE (h:Hashtag { name: hashtag })
-MERGE (t)-[:TAG]->(h)
+    WITH t, event
+    UNWIND event.hashtags as hashtag
+    MERGE (h:Hashtag { name: hashtag })
+    MERGE (t)-[:TAG]->(h)
 
-WITH t, event
-UNWIND event.mentions AS mention
-MERGE (mentionedUser:User { username: mention })
-MERGE (t)-[:MENTIONS]->(mentionedUser)
+    WITH t, event
+    UNWIND event.mentions AS mention
+    MERGE (mentionedUser:User { username: mention })
+    MERGE (t)-[:MENTIONS]->(mentionedUser)
 
-WITH t, event
-UNWIND event.reply_to as reply_to
-MERGE (repUser:User { username: reply_to.username })
-    ON CREATE SET repUser.id = toString(reply_to.user_id)
-MERGE (t)-[:REPLY_TO]->(repUser)
+    WITH t, event
+    UNWIND event.reply_to as reply_to
+    MERGE (repUser:User { username: reply_to.username })
+        ON CREATE SET repUser.id = toString(reply_to.user_id)
+    MERGE (t)-[:REPLY_TO]->(repUser)
 
-WITH t, event
-UNWIND event.urls as url
-MERGE (u:URL { url: url })
-MERGE (t)-[:URL]->(u)
+    WITH t, event
+    UNWIND event.urls as url
+    MERGE (u:URL { url: url })
+    MERGE (t)-[:URL]->(u)
 
-WITH t, event
-UNWIND event.photos as photo
-MERGE (p:URL { url: photo })
-   ON CREATE SET p.photo = true
-MERGE (t)-[:PHOTO]->(p)
-RETURN count(t);
+    WITH t, event
+    UNWIND event.photos as photo
+    MERGE (p:URL { url: photo })
+    ON CREATE SET p.photo = true
+    MERGE (t)-[:PHOTO]->(p)
+    
+    WITH t
+    RETURN count(t) as TweetsMerged;
 """
 
 def load_tweets(driver, batch):
     def tweet_batch(tx):
-        tx.run(LOAD_TWEETS_CYPHER, events=batch)
+        res = tx.run(LOAD_TWEETS_CYPHER, events=batch)
+        rec = res.single()
+        tm = rec.get("TweetsMerged")
+        print("%d tweets merged" % tm)
+        return tm
     
     with driver.session() as session:
-        res = session.write_transaction(tweet_batch)
-        print(res)
-        return res
+        return session.write_transaction(tweet_batch)
 
 def main(argv):
     inputfile = ''
@@ -122,10 +125,17 @@ def main(argv):
     print("Loading tweets from %s" % inputfile)
     batches = load_tweets_in_batches(inputfile)
 
+    b = 0
+
+    total_tweets = 0
+
     for batch in batches:
-        load_tweets(driver, batch)
+        print("Writing batch %d of %d: %d tweets..." % (b, len(batches), len(batch)))
+        tweets_merged = load_tweets(driver, batch)
+        total_tweets = total_tweets + tweets_merged
+
     driver.close()
-    print("Done!")
+    print("Done!  A total of %d tweets merged" % total_tweets)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
